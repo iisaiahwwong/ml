04e83e41e88ad9f498619c26b55b258b89db6494
Isaiah <isaiah.wong@jirehsoho.com>
2017-07-27 16:19:47 -0700

    Start on pandas

---
+++ pandas/pandas.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import pandas as pd
+
+labels = ['a', 'b', 'c']
+my_data = [10, 20, 30]
+
+arr = np.array(my_data)
+
+d = {'a': 10, 'b': 20, 'c': 30}
+
+pd.Series(data = my_data)
+
+pd.Series
