1c372d72794e22fe47679105cc40064d97e5d6d2
iisaiahwwong <isaiah.wong@jirehsoho.com>
2017-07-27 22:50:32 +0800

    dataset  1

--- pandas/dataframes.py
+++ pandas/dataframes.py
@@ -7,4 +7,22 @@ np.random.seed(101)
 
 df = pd.DataFrame(randn(5,4),['A', 'B', 'C', 'D', 'E'],['W', 'X', 'Y', 'Z'])
 
+df['W']
+
+df.W
+
+df[['W', 'Z']]
+
+df['new'] = df['W'] + df['Y']
+
+df.drop('new', axis=1, inplace=True) 
+
+df.drop('E', axis=0)
+
+df.loc['A']
+
+df.iloc[0]
+
+df.loc[['A', 'B'], ['W', 'Y']]
+
 df
