1fe8785b782866a9968c019061a3061b15f64384
Isaiah <isaiah.wong@jirehsoho.com>
2017-08-11 10:34:40 -0700

    Update

--- pandas/dataframes.py
+++ pandas/dataframes.py
@@ -28,7 +28,7 @@ df.iloc[0]
 df.loc[['A', 'B'], ['W', 'Y']]
 
 
-# Dataset part 2
+# Dataframe part 2
 booldf = df > 0
 
 df[df > 0]
@@ -44,11 +44,28 @@ df[(df['W']> 0) & (df['Y'] > 1)]
 index = df.reset_index()
 
 new_index = 'CA NY WY OR '.split()
+test_df = df
+
+if len(new_index) != test_df['W'].shape:
+    new_index.append('SG')
+
+test_df['States'] = new_index
 
 df['States'] = new_index
 
 df.set_index('States')
 
-test_df = df
-
 test_df['W'].shape
+
+#Dataframe part 3
+
+outside = "G1 G1 G1 G2 G2 G2".split()
+inside = list(map(int, "1 2 3 1 2 3".split()))
+hier_index = list(zip(outside, inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+
+df = pd.DataFrame(randn(6,2), hier_index, ['A','B'])
+
+df.loc['G1'].loc[1]
+
+df.index.names = ['Groups', 'Num']
